9e9409d4532af66753fb623e2edbc552f7ed2362
vitalik30446 <36101337+vitalik30446@users.noreply.github.com>
2020-10-04 18:49:13 +0300

    Add files via upload

---
+++ Homework/LW1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+namespace LW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 4;
+            double b = 3;
+            Console.Write("Введите значение r: ");
+            double r = Convert.ToDouble(Console.ReadLine());
+            Console.Write($" r={r} ");
+            double pi = 3.14;
+            Console.Write($" pi={pi} ");
+            double S = 4 * (pi * r * r);
+            Console.Write($" S={S} ");
+            double V = a / b * (pi * r * r * r);
+            Console.Write($" V={V} ");
+        }
+    }
+}
