ddfa83f94705e837257bb8349d01ffb69eb61188
Karasovskiy V <36101337+vitalik30446@users.noreply.github.com>
2020-10-12 10:11:50 +0300

    Create Program.cs

---
+++ Homework/LW2/Program.cs
@@ -0,0 +1,11 @@
+using System;
+namespace LW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(NumericSeries.GetSum(4, 4));
+        }
+    }
+}
