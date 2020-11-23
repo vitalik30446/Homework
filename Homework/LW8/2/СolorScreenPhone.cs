d7818a6fa1fc63786eca447f40595811746af455
Karasovskiy V <36101337+vitalik30446@users.noreply.github.com>
2020-11-23 20:36:34 +0200

    Create СolorScreenPhone.cs

---
+++ Homework/LW8/2/СolorScreenPhone.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LW8._2
+{
+    public class СolorScreenPhone : BlackWhitePhone
+    {
+        public int CountOfColors { get; set; }
+        public bool SupportDualSim { get; set; }
+        public string SecondNumber { get; set; }
+    }
+}
