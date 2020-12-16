173631e502323fd6499c72455116ccbb7120664e
Karasovskiy V <36101337+vitalik30446@users.noreply.github.com>
2020-12-16 11:14:31 +0200

    Update Program.cs

--- Homework/LW4/Program.cs
+++ Homework/LW4/Program.cs
@@ -1,39 +1,63 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
- 
+
 namespace LW4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int n = 10, max = 0, coord_min = 0, min, coord_max = 0, tmp = 0;
+            Console.WriteLine("Дана матрица A(MxN)");
+            Console.Write("M=");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("N=");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            Console.WriteLine("Исходная матрица:");
+            int[,] k = new int[m, n];
             Random r = new Random();
-            int[] mas = new int [n];
-            for (int i = 0; i < n; i++)
-                mas[i] = r.Next(0,100);
- 
-            min = mas[0];
-            for (int i = 0; i < n; i++)
-                Console.Write(mas[i]+ " ");
-            for (int i = 0; i < n; i++)
-                if (mas[i] > max) { max = mas[i]; coord_max = i; }
-            for (int i = 0; i < n; i++)
-                if (mas[i] < min) { min = mas[i]; coord_min = i; }
- 
-            Console.Write("\nmin = "+coord_min + " max = " + coord_max + " ");
- 
-            tmp = mas[coord_min];
-            mas[coord_min] = mas[coord_max];
-            mas[coord_max] = tmp;
-            Console.Write("\n");
-            for (int i = 0; i < n; i++)
-                Console.Write(mas[i] + " ");
- 
+            Console.WriteLine();
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    k[i, j] = r.Next(1, 10);
+                    Console.Write(k[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            int Imax = 0, Jmax = 0, Imin = 0, Jmin = 0;
+            for (int i = 0; i < m; i++)
+            {
+                int max = k[i, 0];
+                int min = k[i, 0];
+                for (int j = 0; j < n; j++)
+                {
+                    if (k[i, j] > max) { max = k[i, j]; Imax = i; Jmax = j; }
+                    if (k[i, j] < min) { min = k[i, j]; Imin = i; Jmin = j; }
+                }
+
+                int z = k[Imax, Jmax];
+                k[Imax, Jmax] = k[Imin, Jmin];
+                k[Imin, Jmin] = z;
+
+                Jmax = 0; Jmin = 0; Imax = i + 1; Imin = i + 1;
+            }
+
+            Console.WriteLine("Преобразованная матрица:");
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(k[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
             Console.ReadKey();
-            
         }
     }
 }
