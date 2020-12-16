c43773ff9e13d3dbed076f7875d25407b3782d05
Karasovskiy V <36101337+vitalik30446@users.noreply.github.com>
2020-12-16 11:35:28 +0200

    Update Program.cs

--- Homework/LW7/Program.cs
+++ Homework/LW7/Program.cs
@@ -9,16 +9,17 @@ namespace LW7
     class Program
     {
         public static void Main(string[] args)
-        {
-            var lists = new List<string> { "4", "34", "765", "325", "234", "8796546", "20", "18", "19"};
+        {    
+            var lists = new List<string> { "1", "2", "3", "10", "120", "5485", "4", "5", "6" };
             var counter = 0;
- 
-            for(int i = 0; i < lists.Count; i++)
+
+            for (int i = 0; i < lists.Count; i++)
             {
                 if (lists.Where(s => s.Length == lists[i].Length).Count() > 1)
                     counter++;
             }
- 
+
+            Console.WriteLine("List: 1 , 2 , 3 , 10 , 120 , 5485 , 4 , 5 , 6 ");
             Console.WriteLine("Result: " + counter);
         }
     }
