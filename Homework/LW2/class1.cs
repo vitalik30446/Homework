369f3e9fe2ce82ac2e9c5d86962000bb3f31af48
Karasovskiy V <36101337+vitalik30446@users.noreply.github.com>
2020-12-17 20:04:24 +0200

    Update and rename Math.cs to class1.cs

--- Homework/LW2/class1.cs
+++ Homework/LW2/class1.cs
@@ -1,17 +1,16 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 namespace LW2
 {
-    public static class NumericSeries
+    public static class class1
     {
         public static double GetSum(double n, double k)
         {
             double res = 0;
             for (int i = 0; i < k; i++)
             {
-                res += (Math.Pow(k, 2) - 3) / ((k * k) * Math.Pow(-1, k)) * k + 3);
-            }
+                res += (Math.Pow(k, 2) - 3) / ((k * k) * Math.Pow(-1, k)) * (k + 3);
+        }
+            return res;
         }
     }
 }
